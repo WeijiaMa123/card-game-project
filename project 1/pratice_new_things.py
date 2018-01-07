18afb5f0b897e062eb33ea1e0c5dcd8e3b562dd7
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2018-01-07 09:43:41 -0500

    almostdone

---
+++ project 1/pratice_new_things.py
@@ -0,0 +1,9 @@
+for i in range(10,1000):
+    while i!=567:
+        i=i+1
+        print("fail")
+        print(i)
+    if i==567:
+        print(i)
+        break
+        
--- project 1/project.py
+++ project 1/project.py
@@ -3,19 +3,19 @@ def deckplayer1(a,b):
     cards=random.randint(a,b)
     pai1=cards%7
     if pai1 == 0 or pai1 ==1:
-        print("attack")
-        decision=int(input("Do player 2 want to use a block card,1.yes  2.no?"))
+        print("from player1 to player2:ATTACK")
         global player2life
+        decision=int(input("Do player 2 want to use a block card,1.yes  2.no?"))
         if decision==1:
             print("player1's attack is blocked")
-            player2life=3
+            player2life=player2life
         else:
             player2life = player2life-1
     elif pai1 == 2 or pai1==3:
         print("block")
 
     elif pai1 == 4:
-        print("heart")
+        print("player1 use HEART")
         global player1life
         if player1life == 3:
             print("player1's maxium life is 3, the heart doesn't work")
@@ -23,9 +23,14 @@ def deckplayer1(a,b):
             player1life=player1life+1
         
     elif pai1 == 5:
-        print("alien invasion")
- 
-        
+        print("form player 1 to player 2: ALIEN INVASION")
+        decision=int(input("Do player2 want to use an attack card,1.yes  2.no"))
+        if decision==1:
+            print("player2 kill all the aliens")
+            player2life = player2life
+        else:
+            print("player2 is invaded by aliens and lose one health")
+            player2life -= 1       
         
     else: 
         print("peace")
@@ -42,6 +47,10 @@ def deckplayer1(a,b):
                 player2life= player2life+1
         else:
             print("player 1 keep a card")
+            
+            
+            
+
            
         
         
@@ -51,12 +60,13 @@ def deckplayer2(a,b):
     pai2=cards%7
 
     if pai2 == 0 or pai2 ==1:
-        print("attack")
+        print("from player2 to player1: ATTACK")
         decision2=int(input("Do player1 want to use a block card,1. yes 2. no"))
+        global player1life
         if decision2== 1:
             print("player2's attack is blocked")
+            player1life=player1life
         else:
-            global player1life
             player1life= player1life-1
     
     elif pai2 == 2 or pai2 ==3:
@@ -64,14 +74,21 @@ def deckplayer2(a,b):
         
         
     elif pai2 == 4:
-        print("heart")
+        print("player2 use HEART")
         global player2life
         if player2life == 3:
             print("player2life is already maxium, the heart don't work")
         else:
             player2life=player2life+1
     elif pai2 == 5:
-        print("alien invasion")
+        print("from player2 to player1:ALIEN INVASION")
+        decision=int(input("do player1 want to use an attack card 1.yes 1.no"))
+
+        if decision==1:
+            player1life=player1life
+        else:
+            print("player1 is invaded and lose 1 health")
+            player1life -= 1
         
     else: 
         print("peace")
@@ -90,13 +107,16 @@ def deckplayer2(a,b):
             print("player 2 keep a card")
         
 
-
         
-    
+print("This is a world of war. Empires are fighting for lands and resources. Now we have 2 army meeting.")
+print("This war is gonna be one of the greatest fight in the human history. The prize for the winner is waiting for you.")
 player1life = 3
 player2life = 3
 
-deckplayer1(1,7)
-deckplayer2(1,7)
-print("player1:",player1life)
-print("player2:",player2life)
+while player1life>=0 or player2life>=0:
+    deckplayer1(1,7)
+    deckplayer1(1,7)
+    deckplayer2(1,7)
+    deckplayer2(1,7)
+    print("player1:",player1life)
+    print("player2:",player2life)
